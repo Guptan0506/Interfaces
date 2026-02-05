89ae1b5bcf8a61ab28854ceb0e0895132a02cfc2
Navya Gupta <navyag0509@gmail.com>
2026-02-05 13:19:23 -0500

    commit init

--- src/BigRectangleFilter.java
+++ src/BigRectangleFilter.java
@@ -1,13 +1,9 @@
-public class BigRectangleFilter {
-    //implements the Filter interface whose accept method accepts all java Rectangle objects that have a perimeter > 10.
+public class BigRectangleFilter implements Filter {
+    @Override
     public boolean accept(Object x) {
         java.awt.Rectangle rect = (java.awt.Rectangle) x;
 
         int perimeter = 2 * (rect.width + rect.height);
-        if (perimeter > 10) {
-            return true;
-        } else {
-            return false;
-        }
+        return perimeter > 10;
     }
 }
